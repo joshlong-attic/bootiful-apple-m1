27def6c8190b0e50b771f9e44c7e6fc2ce7d5106
Josh Long <josh@joshlong.com>
2020-12-23 01:42:57 -0800

    up

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -15,45 +15,44 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @Bean
-    ApplicationListener<ApplicationReadyEvent> ready(
-            DatabaseClient dbc,
-            CustomerRepository cr) {
-        return event -> {
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> ready(DatabaseClient dbc, CustomerRepository cr) {
+		return event -> {
 
-            var sql = """
-                    create table CUSTOMER(
-                        id serial primary key,
-                        name varchar(255) not null
-                    )
-                    """;
+			var sql = """
+					create table CUSTOMER(
+					id serial primary key,
+					name varchar(255) not null
+					)
+					""";
 
-            var ddl = dbc.sql(sql).fetch().rowsUpdated();
+			var ddl = dbc.sql(sql).fetch().rowsUpdated();
 
-            var names = Flux.just("A", "B", "C")
-                    .map(name -> new Customer(null, name))
-                    .flatMap(cr::save);
+			var names = Flux.just("A", "B", "C").map(name -> new Customer(null, name)).flatMap(cr::save);
 
-            var all = cr.findAll();
+			var all = cr.findAll();
 
-            ddl.thenMany(names).thenMany(all).subscribe(System.out::println);
-        };
-    }
+			ddl.thenMany(names).thenMany(all).subscribe(System.out::println);
+		};
+	}
+
 }
 
-
 interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+
 }
 
 @Data
 @AllArgsConstructor
 class Customer {
 
-    @Id
-    public Integer id;
-    public String name;
+	@Id
+	public Integer id;
+
+	public String name;
+
 }
